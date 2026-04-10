f27317859abe6d50c9928962f7d98b65920e4720
Sarah Shakil <sarahshakil2@gmail.com>
2026-04-10 19:28:53 -0400

    updates for transect graphs

--- Analyses/TransectGraphs.R
+++ Analyses/TransectGraphs.R
@@ -1,18 +1,16 @@
 # ===========================================================================================================#
 # TransectGraphs.R
 # By: Sarah Shakil
-# Contact: shakil@ualberta.ca
-# Background: Preliminary stats
-# Last Updated: October 20 2020
-#===========================================================================================================#
+# Background: Publication-ready transect graphs
+# ===========================================================================================================#
 
 ##### (i) Workspace PREP ==============================================================================
 
 ## Clear list 
-list=rm(list=ls(all=TRUE))
+rm(list=ls(all=TRUE))
 
 ## Set working directory
-
+# setwd("YOUR_DIRECTORY_HERE")
 df <- "Data/"
 
 # load libraries
@@ -31,33 +29,28 @@ lengthnona <- function(x) {
   length(x[!is.na(x)])
 }
 
-# load functions
-savePlotpdf <- function(myPlot) {
-  pdf(file = paste0("Figures/", name),
-      width = w, height = h)
-  print(myPlot)
-  dev.off()
-}
-
-# colourblind palette
-
-# The palette with grey:
-cbPalette<-c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2","#D55E00","#CC79A7")
-
-# The palette with black:
-cbbPalette<-c("#000000","#E69F00","#56B4E9","#009E73","#F0E442","#0072B2","#D55E00","#CC79A7")
+# colourblind palettes
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2","#D55E00","#CC79A7")
+cbbPalette <- c("#000000","#E69F00","#56B4E9","#009E73","#F0E442","#0072B2","#D55E00","#CC79A7")
 
 ##### ========== (1) DATA PREP =========================================================================
 
-# (1.2) 2015 Transect
+# (1.1) Watershed Areas
+wa <- read_excel(paste0(df, "watershedareas.xlsx"), sheet="watershedareasforR")
 
+# (1.2) 2015 Transect
 #(1.2.1) POC, Q
 trans2015 <- read_excel(paste0(df, "2015Transects.xlsx"))
-trans2015$POCflux <- trans2015$POCmgL*trans2015$Qm3s*1000
+trans2015$POCflux <- trans2015$POCmgL * trans2015$Qm3s * 1000
 
-cols.num <- c("POCmgL", "Qm3s", "POCflux", "Temp", "Cond")
-trans2015[cols.num] <- sapply(trans2015[cols.num],as.numeric)
-sapply(trans2015, class)
+trans2015 <- merge(trans2015, wa, by.y=c("site", "loc", "trans", "date"),
+                   by.x=c("Slump Site", "Stream Location", "Transect Location", "Sampling Date"),
+                   all.x=TRUE) # FIXED TYPO: changed a..x=TRUE to all.x=TRUE
+
+trans2015$POCyieldmgLkm2 <- (trans2015$POCmgL * trans2015$Qm3s * 1000) / trans2015$WatershedArea
+
+cols.num <- c("POCmgL", "Qm3s", "POCyieldmgLkm2", "Temp", "Cond")
+trans2015[cols.num] <- sapply(trans2015[cols.num], as.numeric)
 
 #(1.2.2) 13C
 c13 <- read_excel(paste0(df, "2015_POC_Amount_13C.xlsx"))
@@ -67,234 +60,134 @@ c13 <- c13[c13$`Multiple Analysis Number`==1,
 
 #(1.2.3) TSS for percPOC
 pc <- read_excel(paste0(df, "2015_POC_TSS.xlsx"))
-pc$TSSmgL <- (pc$`Filter Post-weight (mg)`-pc$`Filter Pre-weight (mg)`)/pc$`Filtered Volume (L)`
+pc$TSSmgL <- (pc$`Filter Post-weight (mg)` - pc$`Filter Pre-weight (mg)`) / pc$`Filtered Volume (L)`
 pc <- pc[,c("Slump Site", "Stream Location", "Transect Location",
             "Sampling Date", "Sample Type", "TSSmgL")]
+
 #(1.2.4) merge
 a <- merge(trans2015, c13, all.x=TRUE)
 trans <- merge(a, pc, all.x=TRUE)
 trans <- trans[trans$`Sample Type`=="Sample",]
 trans$`Sample Type` <- NULL
 
-
 #(1.2.5) New calcs (percPOC)
-trans$percPOC <- trans$POCmgL/trans$TSSmgL
-#trans$POCmgL <- trans$POCmgL/(trans$Qm3s*1000)
+trans$percPOC <- trans$POCmgL / trans$TSSmgL
 
 translong <- trans %>%
-             pivot_longer(cols=c("percPOC", "d13C", "POCmgL", "POCflux"),
-                          names_to="type", values_to = "value")
+  pivot_longer(cols=c("percPOC", "d13C", "POCmgL", "POCyieldmgLkm2"),
+               names_to="type", values_to = "value")
 
 translong$`Sampling Date` <- as.character(translong$`Sampling Date`)
 translong$`Stream Location` <- as.factor(translong$`Stream Location`)
 
-#translongm <- translong %>%
- # group_by(`Slump Site`, `Stream Location`, 
-  #         `Transect Location`, `distm`, `type`) %>%
-  #summarize(n=lengthnona(value), 
-   #         means=mean(value, na.rm=TRUE),
-    #        sd=sd(value, na.rm=TRUE),
-     #       sem=se(value))
-
-#translongm$type <- as.factor(as.character(translongm$type))
-
 # (1.3) 2016 Transect
 data.all <- read_excel(paste0(df, "20162017POCPO14CTrans.xlsx"))
-data.all$distkm <- data.all$distm/1000
+data.all$distkm <- data.all$distm / 1000
 data.samples <- data.all[data.all$`Sample Type`=="Sample",]
 
-##### ========== (2) 2015 Transect Graph =========================================================================
 
-## command to remove scientific notation from ggplot
+##### ========== (2) 2015 Transect Graphing =================================================
+
 options(scipen=999)
+library(patchwork) 
 
-plain <- function(x) {
-  signif(x, 1)
-}
+# 2.1 Universal Theme
+pub_theme <- theme_bw(base_size = 12, base_family = "sans") +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.8),
+    axis.text = element_text(colour = "black"),
+    axis.ticks = element_line(colour = "black"),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    plot.title = element_text(size = 14, face = "bold", hjust = 0.5)
+  )
 
-# save theme
-theme <- theme(panel.background = element_rect(fill="white"), 
-               panel.border = element_rect(colour="black", fill=NA, size=1),
-               plot.margin = unit( c(0.05,0.05,0.05,0.05) , "in"),# fill must equal NA for pane.border or it will block out graph
-               legend.key = element_rect(fill="transparent", colour="transparent"),
-               axis.text=element_text(size=11, colour="black"),
-               axis.title=element_text(size=11, colour="black"),
-               axis.text.x = element_text(angle = 0),
-               legend.position=c(0,1), 
-               legend.justification=c(0, 0),
-               legend.direction="horizontal",
-               legend.background=element_blank(),
-               legend.text=element_text(size=11),
-               legend.spacing.y = unit(0.1, 'cm'),
-               aspect.ratio=0.7,
-               legend.margin = margin(0,0,0,0, unit="cm"),
-               strip.background = element_blank(),
-               strip.placement = "outside") 
+# 2.2 Fix the Legend Dates
+translong <- translong %>%
+  mutate(Sampling_Period = case_when(
+    `Sampling Date` %in% c("2015-07-04", "2015-07-06") ~ "JUL-4 / JUL-6",
+    `Sampling Date` %in% c("2015-07-21", "2015-07-22") ~ "JUL-21 / JUL-22",
+    `Sampling Date` %in% c("2015-08-16", "2015-08-21") ~ "AUG-16 / AUG-21"
+  )) %>%
+  mutate(Sampling_Period = factor(Sampling_Period, 
+                                  levels = c("JUL-4 / JUL-6", "JUL-21 / JUL-22", "AUG-16 / AUG-21")))
 
-# set facet order for graphing
-translong$type <- factor(translong$type, 
-                         levels=c("percPOC", "d13C", "POCmgL", "POCflux"))
+# 2.3 Create Shared Plot Layers 
+base_layers <- list(
+  geom_point(size = 3.5, stroke = 0.8),
+  scale_shape_manual(limits = c("UP", "IN", "DN"), values = c(24, 21, 25), name = "Location:"),
+  scale_fill_manual(limits = c("JUL-4 / JUL-6", "JUL-21 / JUL-22", "AUG-16 / AUG-21"),
+                    values = c("black", "grey50", "white"), name = "Date:",
+                    guide = guide_legend(override.aes = list(shape = 21))),
+  pub_theme,
+  coord_cartesian(clip = "off"), # CRITICAL: Allows text to be drawn outside the plot boundaries
+  theme(plot.margin = margin(t = 20, r = 5, b = 5, l = 5)) # Adds 20pt of space at the top for the labels
+)
 
-# 2.1 SE 2015 Transect =====
+# Helper for hiding X-axis on the top 3 rows
+no_x_axis <- theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
 
-SE <- ggplot() + 
-  #geom_line(data=translong[translong$`Slump Site`=="SE",] ,
-   #         aes(x=distm, y=value,
-    #            colour=as.character(`Sampling Date`)),
-     #       linetype=2) +
-  geom_point(data = translong[translong$`Slump Site`=="SE",], 
-             aes(x=distm, y=value, 
-                 fill=as.character(`Sampling Date`), 
-                 shape=`Stream Location`), 
-             size=4) +
-  scale_shape_manual(breaks=c("UP", "IN", "DN"),
-                     limits=c("UP", "IN", "DN"),
-                     values=c(24, 21, 25),
-                     labels=c("UP", "IN", "DN"),
-                     guide=guide_legend(title=NULL)) +
-  scale_fill_manual(breaks=c("2015-07-06", "2015-07-21", "2015-08-21"),
-                     limits=c("2015-07-06", "2015-07-21", "2015-08-21"),
-                     values=c("black", "grey", "white"),
-                     labels=c("JUL-6", "JUL-21", "AUG-21"),
-                     guide=guide_legend(title=NULL)) +
-  scale_colour_manual(breaks=c("2015-07-06", "2015-07-21", "2015-08-21"),
-                    limits=c("2015-07-06", "2015-07-21", "2015-08-21"),
-                    values=c("black", "grey30", "grey"),
-                    labels=c("JUL-6", "JUL-21", "AUG-21"),
-                    guide=guide_legend(title=NULL)) +
-  ylab (NULL) + xlab("Distance (m)") + 
-  guides(color = guide_legend(override.aes = list(shape = 21))) +
-  facet_wrap(~type, scales="free_y", ncol=1,
-             strip.position = "left", 
-             labeller = as_labeller(c(percPOC = "%POC",
-                                      d13C = expression(delta*"13C"),
-                                      POCmgL = expression("POC conc (mg L"^-1*")"),
-                                      POCflux = expression("POC flux (mg s"^-1*")"))
-             ) ) + theme 
+# --- ROW 1: % POC ---
+p1_SD <- ggplot(translong %>% filter(type == "percPOC", `Slump Site` == "SD"),
+                aes(x = distm, y = value, fill = Sampling_Period, shape = `Stream Location`)) +
+  base_layers + no_x_axis + ggtitle("SD") + ylab("% POC") +
+  annotate("text", x = -Inf, y = Inf, label = "(a)", hjust = 0, vjust = -0.8, size = 5)
 
-SE2 <- ggplot() + 
-  geom_point(data = translong[translong$`Slump Site`=="SE"&
-                              translong$type=="POCmgL",], 
-             aes(x=distm, y=value, 
-                 fill=as.character(`Sampling Date`), 
-                 shape=`Stream Location`), 
-                 size=4) +
-  scale_shape_manual(breaks=c("UP", "IN", "DN"),
-                     limits=c("UP", "IN", "DN"),
-                     values=c(24, 21, 25),
-                     labels=c("UP", "IN", "DN"),
-                     guide=guide_legend(title=NULL)) +
-  scale_fill_manual(breaks=c("2015-07-06", "2015-07-21", "2015-08-21"),
-                    limits=c("2015-07-06", "2015-07-21", "2015-08-21"),
-                    values=c("black", "grey", "white"),
-                    labels=c("JUL-6", "JUL-21", "AUG-21"),
-                    guide=guide_legend(title=NULL)) +
-  scale_colour_manual(breaks=c("2015-07-06", "2015-07-21", "2015-08-21"),
-                      limits=c("2015-07-06", "2015-07-21", "2015-08-21"),
-                      values=c("black", "grey30", "grey"),
-                      labels=c("JUL-6", "JUL-21", "AUG-21"),
-                      guide=guide_legend(title=NULL)) +
-  ylab (NULL) + xlab(NULL) + scale_y_log10() +
-  theme + 
-  theme(legend.position="none",
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+p1_SE <- ggplot(translong %>% filter(type == "percPOC", `Slump Site` == "SE"),
+                aes(x = distm, y = value, fill = Sampling_Period, shape = `Stream Location`)) +
+  base_layers + no_x_axis + ggtitle("SE") + ylab(NULL) +
+  annotate("text", x = -Inf, y = Inf, label = "(b)", hjust = 0, vjust = -0.8, size = 5)
 
-# 2.1 SD 2015 Transect =====
+# --- ROW 2: d13C ---
+p2_SD <- ggplot(translong %>% filter(type == "d13C", `Slump Site` == "SD"),
+                aes(x = distm, y = value, fill = Sampling_Period, shape = `Stream Location`)) +
+  base_layers + no_x_axis + ylab(expression(delta^13*"C-POC")) +
+  annotate("text", x = -Inf, y = Inf, label = "(c)", hjust = 0, vjust = -0.8, size = 5)
 
-# SD
-transSDlong <- translong[translong$`Slump Site`=="SD",] 
+p2_SE <- ggplot(translong %>% filter(type == "d13C", `Slump Site` == "SE"),
+                aes(x = distm, y = value, fill = Sampling_Period, shape = `Stream Location`)) +
+  base_layers + no_x_axis + ylab(NULL) +
+  annotate("text", x = -Inf, y = Inf, label = "(d)", hjust = 0, vjust = -0.8, size = 5)
 
-SD <- ggplot() + 
-  #geom_line(data=transSElong,
-  #         aes(x=distm, y=value,
-  #            colour=as.character(`Sampling Date`)),
-  #       linetype=2) +
-  geom_point(data = translong[translong$`Slump Site`=="SD",], 
-             aes(x=distm, y=value, 
-                 fill=as.character(`Sampling Date`), 
-                 shape=`Stream Location`), 
-             size=4) +
-  scale_shape_manual(breaks=c("UP", "IN", "DN"),
-                     limits=c("UP", "IN", "DN"),
-                     values=c(24, 21, 25),
-                     labels=c("UP", "IN", "DN"),
-                     guide=guide_legend(title=NULL)) +
-  scale_fill_manual(breaks=c("2015-07-04", "2015-07-22", "2015-08-16"),
-                    limits=c("2015-07-04", "2015-07-22", "2015-08-16"),
-                    values=c("black", "grey", "white"),
-                    labels=c("JUL-4", "JUL-22", "AUG-16"),
-                    guide=guide_legend(title=NULL)) +
-  scale_colour_manual(breaks=c("2015-07-04", "2015-07-22", "2015-08-16"),
-                      limits=c("2015-07-04", "2015-07-22", "2015-08-16"),
-                      values=c("black", "grey30", "grey"),
-                      labels=c("JUL-4", "JUL-22", "AUG-16"),
-                      guide=guide_legend(title=NULL)) +
-  ylab (NULL) + xlab("Distance (m)") + 
-  guides(color = guide_legend(override.aes = list(shape = 21))) +
-  facet_wrap(~type, scales="free_y", ncol=1,
-             strip.position = "left", 
-             labeller = as_labeller(c(percPOC = "%POC",
-                                      d13C = expression(delta*"13C"),
-                                      POCmgL = expression("POC conc (mg L"^-1*")"),
-                                      POCflux = expression("POC flux (mg s"^-1*")"))
-             ) )  + theme
+# --- ROW 3: POC mg L-1 (WITH LOG SCALE) ---
+p3_SD <- ggplot(translong %>% filter(type == "POCmgL", `Slump Site` == "SD"),
+                aes(x = distm, y = value, fill = Sampling_Period, shape = `Stream Location`)) +
+  base_layers + no_x_axis + scale_y_log10() + ylab(expression("POC mg L"^-1)) +
+  annotate("text", x = -Inf, y = Inf, label = "(e)", hjust = 0, vjust = -0.8, size = 5)
 
-SD2 <- ggplot() + 
-  geom_point(data = translong[translong$`Slump Site`=="SD"&
-                                translong$type=="POCmgL",], 
-             aes(x=distm, y=value, 
-                 fill=as.character(`Sampling Date`), 
-                 shape=`Stream Location`), 
-             size=4) +
-  scale_shape_manual(breaks=c("UP", "IN", "DN"),
-                     limits=c("UP", "IN", "DN"),
-                     values=c(24, 21, 25),
-                     labels=c("UP", "IN", "DN"),
-                     guide=guide_legend(title=NULL)) +
-  scale_fill_manual(breaks=c("2015-07-04", "2015-07-22", "2015-08-16"),
-                    limits=c("2015-07-04", "2015-07-22", "2015-08-16"),
-                    values=c("black", "grey", "white"),
-                    labels=c("JUL-4", "JUL-22", "AUG-16"),
-                    guide=guide_legend(title=NULL)) +
-  scale_colour_manual(breaks=c("2015-07-04", "2015-07-22", "2015-08-16"),
-                      limits=c("2015-07-04", "2015-07-22", "2015-08-16"),
-                      values=c("black", "grey30", "grey"),
-                      labels=c("JUL-4", "JUL-22", "AUG-16"),
-                      guide=guide_legend(title=NULL)) +
-  ylab (NULL) + xlab(NULL) + scale_y_log10() +
-  theme + 
-  theme(legend.position="none",
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+p3_SE <- ggplot(translong %>% filter(type == "POCmgL", `Slump Site` == "SE"),
+                aes(x = distm, y = value, fill = Sampling_Period, shape = `Stream Location`)) +
+  base_layers + no_x_axis + scale_y_log10() + ylab(NULL) +
+  annotate("text", x = -Inf, y = Inf, label = "(f)", hjust = 0, vjust = -0.8, size = 5)
 
-##### ========== (3) DATA PREP =========================================================================
-### (3.1) load in graphing libraries =====
-library(gtable)
-library(grid)
-library(gridExtra)
+# --- ROW 4: POC yield ---
+p4_SD <- ggplot(translong %>% filter(type == "POCyieldmgLkm2", `Slump Site` == "SD"),
+                aes(x = distm, y = value, fill = Sampling_Period, shape = `Stream Location`)) +
+  base_layers + xlab("Distance (m)") + ylab(expression("POC yield mg s"^-1*"km"^-2)) +
+  annotate("text", x = -Inf, y = Inf, label = "(g)", hjust = 0, vjust = -0.8, size = 5)
 
+p4_SE <- ggplot(translong %>% filter(type == "POCyieldmgLkm2", `Slump Site` == "SE"),
+                aes(x = distm, y = value, fill = Sampling_Period, shape = `Stream Location`)) +
+  base_layers + xlab("Distance (m)") + ylab(NULL) +
+  annotate("text", x = -Inf, y = Inf, label = "(h)", hjust = 0, vjust = -0.8, size = 5)
 
-### (3.2) Graphs and dimensions =====
-SE <- ggplotGrob(SE)
-SD <- ggplotGrob(SD)
+##### ========== (3) COMPILE AND SAVE ====================================================================
 
-### (3.3) Print panel graphs =====
-w = 4; h=11
+if(!dir.exists("Figures")) dir.create("Figures")
 
-name <- "2015SEtransect.pdf"
-savePlotpdf(grid.draw(SE))
+# Stitch the 8 plots together: evaluates Left -> Right, Top -> Bottom
+combined_plot <- (p1_SD | p1_SE) / 
+  (p2_SD | p2_SE) / 
+  (p3_SD | p3_SE) / 
+  (p4_SD | p4_SE) + 
+  plot_layout(guides = "collect") & 
+  theme(legend.position = "top",
+        legend.box = "horizontal",
+        legend.margin = margin(b = 10)) 
 
-name <- "2015SDtransect.pdf"
-savePlotpdf(grid.draw(SD))
-
-
-### (3.4) Print concentration graphs =====
-w = 3.7; h=3.8
-
-name <- "2015SEtransectconc.pdf"
-savePlotpdf(grid.draw(SE2))
-
-w = 3.6; h=3.75
-name <- "2015SDtransectconc.pdf"
-savePlotpdf(grid.draw(SD2))
+# Save the final graphic
+ggsave(filename = "Figures/2015_Transect_Combined.pdf", 
+       plot = combined_plot, 
+       width = 8.5, height = 11, units = "in")
